9a109a6f5f20af07fc166c2f0362bd353e8df6ab
XianNg24 <49993481+XianNg24@users.noreply.github.com>
2023-01-03 11:08:34 +0800

    Add About

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -3,7 +3,12 @@ import React, { Component }  from 'react';
 
 function About() {
     return (
-        <div>This is About</div>
+      <div class="blockquote text-justify" style={{margin: "30px", fontSize: "1.5vw", padding: "10px"}}>
+      <h1> This is a negative news monitoring tool that uses NLP models such as <b>named-entity recognition, sentiment analysis, topic modelling,
+        document similarity, and text summarization</b>. 
+        The news displayed are based on real-world scrapped data from different financial news sources such as Bloomberg, Reuters, and The Wall Street Journal.
+        </h1>
+      </div>
     );
   }
 
